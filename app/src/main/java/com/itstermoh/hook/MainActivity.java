d9a16d29589d8e9cee23bf32287e13e92b5295f0
Muer3ben <78988232+Muer3ben@users.noreply.github.com>
2025-04-16 03:41:51 -0600

    Update MainActivity.java

--- app/src/main/java/com/itstermoh/hook/MainActivity.java
+++ app/src/main/java/com/itstermoh/hook/MainActivity.java
@@ -1,10 +1,10 @@
 package com.itstermoh.hook;
- 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.content.ComponentName;
-import android.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.os.Message;
 import android.os.Handler;
 import java.util.ArrayList;
@@ -13,56 +13,56 @@ import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.TextView;
+import android.widget.ProgressBar;  // <-- Importación de ProgressBar agregada
 
 public class MainActivity extends Activity {
-    
+
     private Blaster blaster;
     private Button test;
-    public TextView bulk;
-    
-    public MainActivity(){};
-     
+    private TextView bulk;
+    private ProgressBar progressBar;  // <-- Declaración del ProgressBar
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         test = findViewById(R.id.test);
         bulk = findViewById(R.id.bulk);
-        
+        progressBar = findViewById(R.id.progressBar); // <-- Inicialización del ProgressBar
+
         blaster = new Blaster(getApplicationContext(), usbHandler);
         blaster.initializeCommunication();
-        
+
         test.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 ArrayList<Byte> arrayList = new ArrayList<>();
-                
+
                 arrayList.add(((byte) -4));
                 arrayList.add(((byte) -4));
                 arrayList.add(((byte) -4));
                 arrayList.add(((byte) -4));
-                
+
                 blaster.test(arrayList);
-                
+
                 if (blaster.sent > 0) {
-                    Toast.makeText(getApplicationContext(), "IR mandato | Size="+blaster.sent, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "IR enviado | Tamaño=" + blaster.sent, Toast.LENGTH_SHORT).show();
                     blaster.sent = 0;
                 } else {
-                    Toast.makeText(getApplicationContext(), "Errore", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-   }
-    
+    }
+
     private final Handler usbHandler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(@NonNull Message msg) {
-        
-        return true;
+            return true;
         }
     });
-    
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -70,7 +70,7 @@ public class MainActivity extends Activity {
             blaster.closeCommunication();
         }
     }
-    
+
     public void setText(String txt) {
         bulk.setText(txt);
     }
